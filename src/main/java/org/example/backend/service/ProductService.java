dac84c4af487aa6e30208f82476e0b01e408c98f
harsh <harsh2401@gmaail.com>
2025-09-15 19:39:18 +0530

    backend part-2

--- src/main/java/org/example/backend/service/ProductService.java
+++ src/main/java/org/example/backend/service/ProductService.java
@@ -4,7 +4,9 @@ import org.example.backend.model.Product;
 import org.example.backend.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -15,5 +17,30 @@ public class ProductService {
 
     public List<Product> getProducts() {
         return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageType(imageFile.getContentType());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int productId, Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int productId) {
+
+        repo.deleteById(productId);
     }
 }
